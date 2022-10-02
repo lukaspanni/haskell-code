4c7a66edc266e172b2cbb64b09f8cdd03fb90087
Lukas Panni <lukas-panni@web.de>
2022-10-02 16:58:04 +0200

    test list functions

--- basics.hs
+++ basics.hs
@@ -4,9 +4,25 @@ quicksort (p:xs) = (quicksort lesser) ++ [p] ++ (quicksort greater)
         lesser = filter (< p) xs
         greater = filter (>= p) xs   
 
-
 testFilterLesser list x = filter (< x) list
 testFilterGreater list x = filter (> x) list
 
-main = putStrLn "Hello World"
+squareMap = map(^2)
+fib x 
+        | x<=1        = 1
+        | otherwise   = fib (x-1) + fib (x-2)
 
+reverseList [] = []
+reverseList (x:xs) = reverseList (xs) ++ [x]
+
+createInverseList x 
+        | x == 1      = 0:[]
+        | otherwise   = (x-1:createInverseList (x-1))
+
+-- [0...n-1]
+createList x = reverseList (createInverseList x)
+
+squareList x = squareMap (createList x)
+
+
+main = putStrLn "Hello World"
