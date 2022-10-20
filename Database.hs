acb8498518cc593cd82bf0c78c301b2ed0043e49
Lukas Panni <lukas.panni@outlook.de>
2022-10-20 11:05:49 +0200

    Database exercise

--- Database.hs
+++ Database.hs
@@ -63,14 +63,15 @@ cons1 =  [("Jane", 111, [141]), ("Jack", 112, [141, 252])]
 
 -- FROM HERE ON, WE WILL ASSUME THAT ALL DATABASES ARE CONSISTENT !!!
 
+testdb = cons1 ++ [("A", 14, [141]), ("Lukas", 12, [141, 252, 126]), ("Peter", 42, [141])]
 
 -- TASK 1
 {-
 Given a database and a student id, we're looking for the list of
 courses of this particular student.
 -}
-query1 :: DB -> Int -> [Int]
-query1 = error "Your code"
+query1 :: DB -> Integer -> [Integer]
+query1 db id = [ c |(n,i,cs) <- db, c <- cs, i==id]
 
 
 -- TASK 2
@@ -78,8 +79,8 @@ query1 = error "Your code"
 Given a database and a course, find all students
 taking this course.
 -}
-query2 :: DB -> Int -> [String]
-query2 = error "Your code"
+query2 :: DB -> Integer -> [String]
+query2 db c = [n | (n,i,cs) <- db, elem c cs] 
 
 
 
@@ -88,8 +89,28 @@ query2 = error "Your code"
 Given a database, sort the database (in non-decreasing order)
 according to the name of students.
 -}
+
+-- sorting using quicksort
 sortDB :: DB -> DB
-sortDB = error "Your code"
+sortDB [] = []
+sortDB ((n,i,c):xs) = (sortDB left) ++ [(n,i,c)] ++ (sortDB right)
+  where
+    left = filter (\(n1,_,_) -> n1 < n) xs
+    right = filter (\(n1,_,_) -> n1 >= n) xs
+
+sortDB' :: DB -> DB
+sortDB' [] = []
+sortDB' ((n,i,cs):xs) = (sortDB' left) ++ [(n,i,cs)] ++ (sortDB' right)
+  where
+    left = filter (\(_,_,cs1) -> (length cs1) < (length cs)) xs
+    right = filter (\(_,_,cs1) -> (length cs1) >= (length cs)) xs
+
+sortDB'' :: (Student -> Student -> Ordering) -> DB -> DB
+sortDB'' cmp [] = []
+sortDB'' cmp (x:xs) = (sortDB'' cmp left) ++ [x] ++ (sortDB'' cmp right)
+  where
+    left = filter (\y -> cmp y x == LT) xs
+    right = filter (\y -> cmp y x /= LT) xs
 
 {-
 Extension1:
@@ -98,7 +119,8 @@ Provide a function sortDB' which sorts the database according to the number of c
 Extension2:
 Provide a function sortDB'' which is parameterized in the actual comparing relation which determines the sorting order
 For example:
- Given
+ Given 
+-}
 
 cmpName :: Student -> Student -> Ordering
 cmpName (n1, _, _) (n2, _, _) =
@@ -108,6 +130,7 @@ cmpName (n1, _, _) (n2, _, _) =
       then GT
       else EQ
 
+{-
 Then you can define
 
  sortDB = sortDB'' cmpName
@@ -125,5 +148,35 @@ Given two databases, merge them to obtain one (consistent) database
 
 -}
 
+mergedb1 = [("Jane", 112, [141, 353])]
+mergedb2 = [("Jane", 112, [141, 252])]
+
+mergeCourses :: [Integer] -> [Integer] -> [Integer]
+mergeCourses [] cs = cs
+mergeCourses cs [] = cs
+mergeCourses (c1:cs1) cs2 = c1:mergeCourses cs1 (filter (\x -> x /= c1) cs2)
+
 merge :: DB -> DB -> DB
-merge = error "Your code"
+merge [] db = db
+merge db [] = db
+merge (s1:db1) db2 = (update s1 db2):(merge db1 (filtered s1 db2))
+  where
+    filtered (_,i,_) xs = filter (\(_,i1,_) -> i /= i1) xs
+    update (n,i,c) xs = mergeStudent (n,i,c) (filter (\(_,i1,_) -> i==i1) xs)
+    mergeStudent (n,i,c) [(n1,i1,c1)] = (n,i,(mergeCourses c c1))
+
+
+merge2 [] db = db
+merge2 db [] = db
+merge2 (s1:db1) db2 = update s1 ++ (merge2 db1 (filtered s1 db2))
+  where 
+    update (n,i, c) = [(n,i,(mergeCourses2 c c1)) | (_,i1,c1) <- db2, i1 == i]
+      where 
+        mergeCourses2 cs1 cs2
+          | null cs1 = cs2
+          | null cs2 = cs1
+          | otherwise = (head cs1):mergeCourses (tail cs1) (filter (\x -> x /= (head cs1)) cs2)
+    filtered (_,i,_) db = [ (n,i1,c) | (n,i1,c) <- db, i1 /= i]
+
+
+    
---
+++ algorithms.hs
@@ -0,0 +1,40 @@
+quicksort [] = []
+quicksort (x:xs) = (quicksort left) ++ [x] ++ (quicksort right)
+  where
+    left = filter (<x) xs
+    right = filter (>=x) xs
+
+
+mergesort [x] = [x]
+mergesort xs = merge (mergesort left) (mergesort right)
+  where
+    (left,right) = split xs
+
+
+
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys)
+  | x < y = x:merge xs (y:ys)
+  | otherwise = y:merge (x:xs) ys
+
+split xs = (left, right)
+  where
+    left = take size xs
+    right = drop size xs
+    size = (length xs) `div` 2
+
+
+
+count [] = 0
+count (x:xs) = (count xs) + 1
+
+myFilter _ [] = []
+myFilter pred (x:xs)
+  | pred x = x:myFilter pred xs
+  | otherwise = myFilter pred xs
+
+countIf _ [] = 0
+countIf pred (x:xs) = (countIf pred xs) + inc
+  where
+    inc = if pred x then 1 else 0
