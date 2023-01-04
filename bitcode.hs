a94c4efd3d297e7fe8bb8577e40976859351bba5
Lukas Panni <lukas.panni@outlook.de>
2023-01-04 11:02:30 +0100

    bitcoded characters

---
+++ bitcode.hs
@@ -0,0 +1,60 @@
+data Tree = Leaf Char | Node Tree Tree
+
+data Code = Zero | One deriving (Show, Eq)
+
+type Table = [(Char, [Code])]
+
+codeTable :: Tree -> Table
+codeTable (Leaf c) = [(c, [])]
+codeTable (Node l r) = 
+  let tablel = codeTable l
+      tabler = codeTable r
+  in (map (\(c,cd) -> (c, Zero:cd)) tablel ++ map (\(c,cd) -> (c, One:cd)) tabler)
+
+
+t1 = Node (Leaf 'a') (Node (Leaf 'b') (Leaf 'c'))
+
+t1Code = codeTable t1
+
+retrieve :: Table -> Char -> [Code]
+retrieve [] _ = []
+retrieve t c = [cc | (x,cd) <- t, x==c, cc <- cd]
+
+
+retrieve' t c = (map (\(c,code) -> code) $ filter (\(c1,_) -> c1 == c) t)!!0
+
+
+encode :: Tree -> [Char] -> [Code]
+encode t [] = []
+encode t (x:xs) = (retrieve (codeTable t) x) ++ (encode t xs)
+
+
+valid :: Tree -> Bool
+valid t = hasNoDups $ [c | (c,_) <- codeTable t]
+  where
+    hasNoDups [] = True
+    hasNoDups (x:xs)
+      | elem x xs = False
+      | otherwise = hasNoDups xs 
+
+
+decode :: Tree -> [Code] -> [Char]
+decode t [] = []
+decode t ls = decode' t (decodeC t ls)
+  where
+    decode' t (c,[]) = [c]
+    decode' t (c,ls) = c:(decode' t (decodeC t ls))
+    decodeC (Leaf c) ls = (c,ls)
+    decodeC (Node l r) (x:xs)
+      | x == One = decodeC r xs
+      | otherwise = decodeC l xs
+
+
+simplerDecode :: Tree -> [Code] -> [Char]
+simplerDecode t ls = decodeI t t ls
+  where
+    decodeI t _ [] = []
+    decodeI t (Leaf c) ls = c:(decodeI t t ls)
+    decodeI t (Node l r) (x:xs)
+      | x == One = decodeI t r xs
+      | otherwise = decodeI t l xs 
